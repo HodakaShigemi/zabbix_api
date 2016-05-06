ab20e4c1a07cafecfe89033ce3ee7586c3a730f4
HodakaShigemi <japaneselegroll@yahoo.co.jp>
2016-05-06 13:46:20 +0900

    added graph images saving method

--- ReportServer.py
+++ ReportServer.py
@@ -1,6 +1,7 @@
 from pyzabbix import ZabbixAPI
 from datetime import datetime
 import re, time, pandas
+import dateutil.parser
 
 class GenerateReport(object):
     """
@@ -12,7 +13,8 @@ class GenerateReport(object):
         初期設定を行う
         デフォルトでサーバのURLはローカルのzabbixサーバになっている
         """
-        self.zapi = ZabbixAPI(server = server)
+        self.server = server
+        self.zapi = ZabbixAPI(server = self.server)
         self.hosts_dictionary = {}
 
     def update_hosts_dictionary(self):
@@ -29,8 +31,9 @@ class GenerateReport(object):
         この関数はホストのhostidを必要とする
         辞書は {"アイテムのkey":"アイテムID",....}となっている
         """
-        items_list = self.zapi.item.get(hostids=hostid, output=["key_"])
         items_dictionary_of_host = {}
+        items_list = self.zapi.item.get(hostids=hostid, output=["key_"])
+
         for item in items_list:
             items_dictionary_of_host[item["key_"]] = item["itemid"]
         return items_dictionary_of_host
@@ -48,12 +51,7 @@ class GenerateReport(object):
         elif time_string == 'this_month':
             time = datetime(now.year, now.month, 1)
         else:
-            time_splited = re.split(r'¥W', time_string)
-            for i in range(len(time_splited)):
-                time_splited[i] = int(time_splited[i])
-            while len(time_splited) < 3:
-                time_splited.append(1)
-            time = datetime(*time_splited)
+            time = dateutil.parser.parse(time_string)
         Unix_time = int(time.timestamp())
         return Unix_time
 
@@ -76,6 +74,55 @@ class GenerateReport(object):
         history_dataframe = pandas.DataFrame(history_of_item)
         history_dataframe['clock', 'value'].to_csv(name_saving_file, mode = 'a')
 
+    def save_graph_image(self, graphid, time_from, time_till, width=1600, height=900, save_as='graph.png'):
+        """
+        グラフの画像を保存する。
+        グラフのID、グラフにする期間の始まりと終わりを必要とする。
+        オプションで、グラフの縦・横のサイズ、画像ファイルの名前を指定できる。
+        返す値は保存したグラフ画像の相対パス。
+        """
+        period = self.string_to_Unix_time(time_till) - self.string_to_Unix_time(time_from)
+        time_from_as_datetime = dateutil.parser.parse(time_from)
+        parameters = {'graphid':graphid, 'width':width, 'height':height, 'stime':time_from_as_datetime.strftime("%Y%m%d%H%M%S"), 'period':period }
+        response = request.get(self.server + "/chart2.php", params = parameters, cookies = {'zbx_sessionid':self.zapi.auth},)
+        file = open(save_as, 'wb')
+        file.write(response.content)
+        file.close()
+        return save_as
+
+    def save_graph_images_with(self, hostid, search_word, time_from, time_till):
+        """
+        特定ホストのグラフについて、名前にsearch_wordを含むものをすべて保存する。
+        返す値は、保存したグラフ画像の相対パスのリスト。
+        """
+        saved_graph_pathes = []
+        for graph in self.zapi.graph.get(hostids = [host_id], search = {'name':[search_word]}, output = ['name']):
+            graph_name = graph['name']
+            graph_id = graph['graphid']
+            saved_graph_pathes.appned(self.save_graph_images(graphid = graph_id, time_from = time_from, time_till = time_till, saveas = graph_name))
+        return saved_graph_pathes
+
+    def make_report(self, kk_name, time_from = 'last_month', time_till = 'this_month'):
+        """
+        特定の加入機関のレポートを作成する。
+        kk_nameが加入機関の名前、レポートにする期間についても指定可能（デフォルトで先月の一ヶ月間）
+        """
+        time_from = string_to_Unix_time(time_from)
+        time_till = string_to_Unix_time(time_till)
+        hosts_of_kk = {}
+        saved_graph_names = []
+        for host in self.zapi.host.get(output=['host', 'name'], search = {'name':kk_name}):
+            hosts_of_kk[host['name']] = host['hostid']
+
+        for host_name, host_id in hosts_of_kk.items():
+            graph_images = []
+            if 'kk' in host_name:
+                graph_images.extend(self.save_graph_images_with(hostid = host_id, search_word = 'traffic', time_from = time_from, time_till = time_till))
+            graph_images.extend(self.save_graph_images_with(hostid = host_id, search_word = 'optpower', time_from = time_from, time_till = time_till)) 
+        graph_images.reverse()
+        while graph_images:
+            paste_to_paper(graph_images.pop()) #レポートのフォーマットに画像を貼り付けていく処理
+        return report_name
 
 class Server(object):
     """
