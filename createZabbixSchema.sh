b8191d468b364320f33e135c17e732c80ffda31e
重見穂高 <japaneselegroll@yahoo.co.jp>
2016-10-02 14:25:05 +0900

    clean up directories

--- createZabbixSchema.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-zabbixPath=`ls -d /usr/share/doc/* | grep -e"zabbix-server-mysql-.*"`
-eval /usr/bin/mysql -uroot zabbix < ${zabbixPath}/create/data.sql
-eval /usr/bin/mysql -uroot zabbix < ${zabbixPath}/create/data.sql
-eval /usr/bin/mysql -uroot zabbix < ${zabbixPath}/create/data.sql
